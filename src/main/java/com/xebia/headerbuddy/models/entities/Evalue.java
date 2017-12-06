5d3f2b08a74d1e757a0708f49b393951ce547f9e
Andono <jordyberg@gmail.com>
2017-12-06 16:40:00 +0100

    Fixed bug in class Evalue

--- src/main/java/com/xebia/headerbuddy/models/entities/Evalue.java
+++ src/main/java/com/xebia/headerbuddy/models/entities/Evalue.java
@@ -15,6 +15,22 @@ public class Evalue {
 
     private String description;
 
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @ManyToMany(mappedBy = "values")
     public Set<Ereport> reports;
 
