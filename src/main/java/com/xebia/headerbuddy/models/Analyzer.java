fef5cd559f0e9af4a34863339bb982c580c85116
Andono <jordyberg@gmail.com>
2017-12-21 14:59:17 +0100

    Bugfix duplicated missing do headers

--- src/main/java/com/xebia/headerbuddy/models/Analyzer.java
+++ src/main/java/com/xebia/headerbuddy/models/Analyzer.java
@@ -3,7 +3,9 @@ package com.xebia.headerbuddy.models;
 import com.xebia.headerbuddy.models.entities.Ereport;
 import com.xebia.headerbuddy.models.entities.Euser;
 import com.xebia.headerbuddy.models.entities.Evalue;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Analyzer {
@@ -25,55 +27,10 @@ public class Analyzer {
 
         this.foundValues = foundValues;
         organiseValuesByCategory(databaseHeaderValues);
-
-        // =================================================
-        // ============== Printing out data ================
-        // =================================================
-//        System.out.println("========= Found values ============");
-//        for (Evalue value: foundValues){
-//            System.out.println("Value: " + value.getValue() + ", Header: " + value.getHeader().getName());
-//        }
-//
-//        System.out.println("========= Database Do values ============");
-//        for (Evalue value: databaseDoValues){
-//            System.out.println("Value: " + value.getValue() + ", Header: " + value.getHeader().getName() + ", Category: " + value.getCategory().getName());
-//        }
-//
-//        System.out.println("========= Database Don't values ============");
-//        for (Evalue value: databaseDontValues){
-//            System.out.println("Value: " + value.getValue() + ", Header: " + value.getHeader().getName() + ", Category: " + value.getCategory().getName());
-//        }
-//
-//        System.out.println("========= Database Rec values ============");
-//        for (Evalue value: databaseRecValues){
-//            System.out.println("Value: " + value.getValue() + ", Header: " + value.getHeader().getName() + ", Category: " + value.getCategory().getName());
-//        }
-//
-//        System.out.println();
-//        System.out.println("========= Missing Do values ============");
-//        for (Evalue value: missingDoValues){
-//            System.out.println("Header: " + value.getHeader().getName() + ", Category: " + value.getCategory().getName());
-//        }
-//
-//        System.out.println("========= Found Do values ============");
-//        for (Evalue value: foundDoValues){
-//            System.out.println("Value: " + value.getValue() + ", Header: " + value.getHeader().getName());
-//        }
-//
-//        System.out.println("========= Found Don't values ============");
-//        for (Evalue value: foundDontValues){
-//            System.out.println("Header: " + value.getHeader().getName());
-//        }
-//
-//        System.out.println("========= Found Rec values ============");
-//        for (Evalue value: foundRecValues){
-//            System.out.println("Value: " + value.getValue() + ", of header: " + value.getHeader().getName() + ", Description: " + value.getDescription());
-//        }
-
     }
 
     //This method does the analyses
-    public Ereport analyseHeaders(Euser user){
+    public Ereport analyseHeaders(Euser user) {
         missingDoValues = detectMissingDoHeaders(foundValues, databaseDoValues);
         foundDoValues = detectDoHeaders(foundValues, databaseDoValues);
         foundDontValues = detectDontHeaders(foundValues, databaseDontValues);
@@ -85,41 +42,47 @@ public class Analyzer {
         reportValues.addAll(foundDontValues);
         reportValues.addAll(foundRecValues);
 
-
         Ereport report = new Ereport(user, reportValues);
 
         return report;
     }
 
-    private Set<Evalue> detectMissingDoHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> doValues){
+    private Set<Evalue> detectMissingDoHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> doValues) {
         Set<Evalue> missingDoValues = new HashSet<>();
+        List<String> missingHeaderNames = new ArrayList<>();
 
-        for (Evalue doValue : doValues){
+
+        for (Evalue doValue : doValues) {
             boolean found = false;
 
-            for (Evalue analyseValue : toAnalyseValues){
+            //Making sure that already detected missing headers are not checked again
+            if(!missingHeaderNames.contains(doValue.getHeader().getName())) {
 
-                if (analyseValue.getHeader().getName().equals(doValue.getHeader().getName())){
-                    found = true;
-                    break;
+                for (Evalue analyseValue : toAnalyseValues) {
+
+                    if (analyseValue.getHeader().getName().equals(doValue.getHeader().getName())) {
+                        found = true;
+                        break;
+                    }
                 }
-            }
 
-            if (!found){
-                missingDoValues.add(doValue);
+                if (!found) {
+                    missingDoValues.add(doValue);
+                    missingHeaderNames.add(doValue.getHeader().getName());
+                }
             }
         }
 
         return missingDoValues;
     }
 
-    private Set<Evalue> detectDoHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> doValues){
+    private Set<Evalue> detectDoHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> doValues) {
         Set<Evalue> foundDoValues = new HashSet<>();
 
-        for (Evalue analyseValue : toAnalyseValues){
-            for (Evalue doValue : doValues){
+        for (Evalue analyseValue : toAnalyseValues) {
+            for (Evalue doValue : doValues) {
 
-                if(analyseValue.getHeader().getName().equals(doValue.getHeader().getName())){
+                if (analyseValue.getHeader().getName().equals(doValue.getHeader().getName())) {
                     foundDoValues.add(analyseValue);
                     break;
                 }
@@ -129,13 +92,13 @@ public class Analyzer {
         return foundDoValues;
     }
 
-    private Set<Evalue> detectDontHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> dontValues){
+    private Set<Evalue> detectDontHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> dontValues) {
         Set<Evalue> foundDontValues = new HashSet<>();
 
-        for (Evalue analyseValue : toAnalyseValues){
-            for (Evalue dontValue : dontValues){
+        for (Evalue analyseValue : toAnalyseValues) {
+            for (Evalue dontValue : dontValues) {
 
-                if (analyseValue.getHeader().getName().equals(dontValue.getHeader().getName())){
+                if (analyseValue.getHeader().getName().equals(dontValue.getHeader().getName())) {
                     foundDontValues.add(dontValue);
                     break;
                 }
@@ -145,16 +108,16 @@ public class Analyzer {
         return foundDontValues;
     }
 
-    private Set<Evalue> detectRecHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> recValues){
+    private Set<Evalue> detectRecHeaders(Set<Evalue> toAnalyseValues, Set<Evalue> recValues) {
         Set<Evalue> foundRecValues = new HashSet<>();
 
-        for (Evalue analyseValue : toAnalyseValues){
-            for (Evalue recValue : recValues){
+        for (Evalue analyseValue : toAnalyseValues) {
+            for (Evalue recValue : recValues) {
 
                 //Header and value needs to be the same
-                if(analyseValue.getHeader().getName().equals(recValue.getHeader().getName()) && analyseValue.getValue().equals(recValue.getValue())){
-                   foundRecValues.add(recValue);
-                   break;
+                if (analyseValue.getHeader().getName().equals(recValue.getHeader().getName()) && analyseValue.getValue().equals(recValue.getValue())) {
+                    foundRecValues.add(recValue);
+                    break;
                 }
             }
         }
@@ -162,13 +125,13 @@ public class Analyzer {
         return foundRecValues;
     }
 
-    private void organiseValuesByCategory(Iterable<Evalue> values){
-        for (Evalue value : values){
-            if (value.getCategory().getName().equals("do")){
+    private void organiseValuesByCategory(Iterable<Evalue> values) {
+        for (Evalue value : values) {
+            if (value.getCategory().getName().equals("do")) {
                 databaseDoValues.add(value);
-            } else if (value.getCategory().getName().equals("dont")){
+            } else if (value.getCategory().getName().equals("dont")) {
                 databaseDontValues.add(value);
-            } else if (value.getCategory().getName().equals("recommendation")){
+            } else if (value.getCategory().getName().equals("recommendation")) {
                 databaseRecValues.add(value);
             }
         }
