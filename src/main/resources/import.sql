ada806cca8882c775deebca768e7cb152a4e733c
Nick Kronenberg <nikro412956@gmail.com>
2017-12-21 14:37:15 +0100

    added more csp directives

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -50,6 +50,24 @@ INSERT INTO value (value, description, header_id, category_id) VALUES ("connect-
 INSERT INTO value (value, description, header_id, category_id) VALUES ("child-src", "Defines the valid sources for web workers and nested browsing contexts loaded using elements such as <frame> and <iframe>. Don't use this because it is depricated", 1, 2);
 INSERT INTO value (value, description, header_id, category_id) VALUES ("font-src", "Specifies valid sources for fonts loaded using @font-face.", 1, 1);
 INSERT INTO value (value, description, header_id, category_id) VALUES ("frame-src", "Specifies valid sources for nested browsing contexts loading using elements such as <frame> and <iframe>.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("img-src", "Specifies valid sources of images and favicons.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("manifest-src", "Specifies valid sources of application manifest files.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("media-src", "Specifies valid sources for loading media using the <audio> , <video> and <track> elements.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("object-src", "Specifies valid sources for the <object>, <embed>, and <applet> elements.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("script-src", "Specifies valid sources for JavaScript.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("style-src", "Specifies valid sources for stylesheets.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("worker-src", "Specifies valid sources for Worker, SharedWorker, or ServiceWorker scripts.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("base-uri", "Restricts the URLs which can be used in a document's <base> element.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("plugin-types", "Restricts the set of plugins that can be embedded into a document by limiting the types of resources which can be loaded.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("sandbox", "Enables a sandbox for the requested resource similar to the <iframe> sandbox attribute.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("disown-opener", "Ensures a resource will disown its opener when navigated to. This should not be used because it is still expirimental and may not work as intended.", 1, 2);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("form-action", "Restricts the URLs which can be used as the target of a form submissions from a given context.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("frame-ancestors", "Specifies valid parents that may embed a page using <frame>, <iframe>, <object>, <embed>, or <applet>.", 1, 1);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("navigation-to", "Restricts the URLs to which a document can navigate by any means (a, form, window.location, window.open, etc.). This should not be used because it is expirimental and may not work as intended.", 1, 2);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("report-uri", "Instructs the user agent to report attempts to violate the Content Security Policy. These violation reports consist of JSON documents sent via an HTTP POST request to the specified URI. This should be used together with the report-to directive", 1, 3);
+INSERT INTO value (value, description, header_id, category_id) VALUES ("report-to", "Fires a SecurityPolicyViolationEvent. This should be used with the 'report-uri' directive because not all browsers support this directive. if this directive is supported it will ignore report-uri", 1, 3);
+
+
 
 # =========== Referrer-Policy =============
 INSERT INTO value (value, description, header_id, category_id) VALUES ("no-referrer", "The Referer header will be omitted entirely. No referrer information is sent along with requests.", 2, 1);
