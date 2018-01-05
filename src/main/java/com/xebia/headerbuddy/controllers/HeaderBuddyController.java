d75c50d48761892b38f4ef4b294340938213f2a5
OldaranT <timbaak@hotmail.com>
2018-01-05 10:28:29 +0100

    Resolved Checkstyle & findbugs errors.

--- src/main/java/com/xebia/headerbuddy/controllers/HeaderBuddyController.java
+++ src/main/java/com/xebia/headerbuddy/controllers/HeaderBuddyController.java
@@ -8,8 +8,17 @@ import com.xebia.headerbuddy.models.entities.Ereport;
 import com.xebia.headerbuddy.models.entities.Eurl;
 import com.xebia.headerbuddy.models.entities.Euser;
 import com.xebia.headerbuddy.models.entities.Evalue;
-import com.xebia.headerbuddy.models.entities.repositories.*;
-import com.xebia.headerbuddy.utilities.*;
+import com.xebia.headerbuddy.models.entities.repositories.EvalueRepository;
+import com.xebia.headerbuddy.models.entities.repositories.EprofileRepository;
+import com.xebia.headerbuddy.models.entities.repositories.EuserRepository;
+import com.xebia.headerbuddy.models.entities.repositories.EreportRepository;
+import com.xebia.headerbuddy.models.entities.repositories.EurlRepository;
+import com.xebia.headerbuddy.utilities.WebCrawler;
+import com.xebia.headerbuddy.utilities.ProtocolHandler;
+import com.xebia.headerbuddy.utilities.MethodHandler;
+import com.xebia.headerbuddy.utilities.UrlSerializer;
+import com.xebia.headerbuddy.utilities.ValueSerializer;
+import com.xebia.headerbuddy.utilities.APIKeyGenerator;
 import com.xebia.headerbuddy.annotations.ValidAPIKey;
 import com.xebia.headerbuddy.annotations.ValidEmail;
 import com.xebia.headerbuddy.annotations.ValidMethod;
@@ -21,6 +30,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -47,10 +57,10 @@ public class HeaderBuddyController {
 
     @RequestMapping(value = "/headerbuddy/api")
     public ResponseEntity headerBuddy(@RequestParam(value = "url", required = true) @ValidURL String url,
-                                              @RequestParam(value = "key", required = true) @ValidAPIKey String key,
-                                              @RequestParam(value = "output", defaultValue = "json", required = false) @ValidOutput String output,
-                                              @RequestParam(value = "method", defaultValue = "get", required = false) @ValidMethod String method,
-                                              @RequestParam(value = "crawl", defaultValue = "false", required = false) boolean crawl) throws Exception {
+                                      @RequestParam(value = "key", required = true) @ValidAPIKey String key,
+                                      @RequestParam(value = "output", defaultValue = "json", required = false) @ValidOutput String output,
+                                      @RequestParam(value = "method", defaultValue = "get", required = false) @ValidMethod String method,
+                                      @RequestParam(value = "crawl", defaultValue = "false", required = false) boolean crawl) throws Exception {
 
         List<Header> foundHeaders = new ArrayList<>();
         Set<String> visitedPages = new HashSet<>();
